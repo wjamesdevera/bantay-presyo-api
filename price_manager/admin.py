2c54f56c35f286ad6d4240ee4b6f205b4a73c83e
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-20 16:12:39 +0800

    add commodity models

--- price_manager/admin.py
+++ price_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Commodity, CommodityPrices
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Commodity)
+admin.site.register(CommodityPrices)
--- price_manager/models.py
+++ price_manager/models.py
@@ -1,26 +1,56 @@
 from django.db import models
+import datetime
+import uuid
 
 # Create your models here.
 
 
 class Category(models.Model):
+    category_id = models.AutoField(
+        primary_key=True, editable=False, unique=True)
     name = models.CharField(max_length=200)
 
     class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
 
 class Commodity(models.Model):
+    commodity_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     specification = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=11)
-    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "commodity"
+        verbose_name_plural = "commodities"
+
     def __str__(self):
-        return self.name
+        return f"{self.category.name} - {self.name}"
+
+
+class CommodityPrices(models.Model):
+    commodity_price_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    commodity = models.ForeignKey(Commodity, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=11)
+    date = models.DateField(default=datetime.datetime.now())
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "commodities_prices"
+        verbose_name = "commodities_price"
+        verbose_name_plural = "commodities_prices"
+
+    def __str__(self):
+        return f"{self.commodity} - {self.date}"
