eb40143531fd2c80d35f85f34a25c022161fabf7
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-21 20:38:33 +0800

    transition to fastAPI

--- price_manager/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Commodity, CommodityPrices
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Commodity)
-admin.site.register(CommodityPrices)
