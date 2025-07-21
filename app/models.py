a787ab4f3da0d5de0a65b7dd7e9f271f767b5f4e
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-21 21:57:19 +0800

    created category model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.database import engine
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, DateTime, func
+from sqlalchemy import Integer, DateTime, func, String
 from datetime import datetime
+
+database_file = "db.sqlite3"
+sql_database_url = f"sqlite:///{database_file}"
+
+engine = create_engine(sql_database_url, echo=True)
+
+Session = sessionmaker(bind=engine)
 
 
 class BaseModel(DeclarativeBase):
@@ -12,6 +20,10 @@ class BaseModel(DeclarativeBase):
         DateTime(), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(), onupdate=func.now())
+
+
+class Category(BaseModel):
+    name: Mapped[str] = mapped_column(String(200))
 
 
 BaseModel.metadata.create_all(bind=engine)
--- app/models/category.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseModel
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
-
-
-class Category(BaseModel):
-    name: Mapped[str] = mapped_column(String(200))
