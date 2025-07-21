07192a0fa787f49e824d0adb865192d60968e1dd
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-21 23:47:26 +0800

    remove unused database file

--- app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
-database_file = "db.sqlite3"
-sql_database_url = f"sqlite:///{database_file}"
-
-engine = create_engine(sql_database_url, echo=True)
-
-Session = sessionmaker(bind=engine)
