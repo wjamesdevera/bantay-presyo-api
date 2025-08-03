f4ebf42dc71f8db2a2a030625b3ec6c9c547ded1
w.jamesdevera <w.jamesdevera@gmail.com>
2025-08-03 20:27:09 +0800

    Update commodity and category model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, String
 from datetime import datetime
 import uuid
 
 
 class Category(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
+    name: str = Field(index=True, max_length=200)
     created_at: datetime
     updated_at: datetime
 
@@ -14,8 +14,9 @@ class Category(SQLModel, table=True):
 
 class Commodity(SQLModel, table=True):
     id: str | None = Field(default=uuid.uuid4, primary_key=True)
-    name: str = Field(index=True)
-    category_id: int | None = Field(default=None, foreign_key="category.id")
+    name: str = Field(index=True, max_length=200)
+    specification: str | None = Field(max_length=200, default=None)
+    category_id: int | None = Field(foreign_key="category.id")
     category: Category | None = Relationship(back_populates="commodities")
     created_at: datetime
     updated_at: datetime
